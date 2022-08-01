a0818e152ab0cb47968ebcd3de744ef73488b6ca
Tim Simms <tsimms@gmail.com>
2022-08-01 15:44:38 -0500

    count height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,14 +1,15 @@
 #include "binary_trees.h"
 
 /**
- *	binary_tree_heigt - measures height of a binary tree
- * @tree pointer to root node
- * return: height or 0
+ * binary_tree_height - measures height of a binary tree
+ * @tree: pointer to root node
+ * Return: height or 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left = 0;
 	size_t right = 0;
+
 	if (!tree)
 		return (0);
 	if (tree->left)
@@ -17,6 +18,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		right = 1 + binary_tree_height(tree->right);
 	if (left >= right)
 		return (left);
-	return(right);
+	return (right);
 
 }
